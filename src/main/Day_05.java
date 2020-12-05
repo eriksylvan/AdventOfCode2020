e3203f0e3341b8ddb82441ced4f436f868c5b56d
Sylvan <sylver@corp.delaval.com>
2020-12-05 14:30:21 +0100

    Solution Day 05 Part one

---
+++ src/main/Day_05.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Day_05
+ */
+public class Day_05 {
+
+    String inputFile = "input\\input_05.txt";
+    ArrayList<String> seatsBinary = new ArrayList<String>();
+
+    public void getInputData() {
+        Scanner inputScanner;
+        inputScanner = new Scanner(Day_04.class.getResourceAsStream(inputFile));
+        this.seatsBinary = new ArrayList<>();
+        while (inputScanner.hasNextLine()) {
+            seatsBinary.add(inputScanner.nextLine());
+        }
+        inputScanner.close();
+
+    }
+
+    public int day05PartOne() {
+        int maxSeatID = -1;
+        for (String seat : this.seatsBinary) {
+            int seatID = seatId(seat);
+            maxSeatID = (seatID > maxSeatID) ? seatID : maxSeatID;
+        }
+
+        return maxSeatID;
+    }
+
+    public int seatId(String seat) {
+        String row = seat.substring(0, 7);
+        String col = seat.substring(7, 10);
+        row = row.replace('F', '0').replace('B', '1');
+        col = col.replace('L', '0').replace('R', '1');
+
+        int rowDec = Integer.parseInt(row, 2);
+        int colDec = Integer.parseInt(col, 2);
+        int seatID = rowDec * 8 + colDec;
+        return seatID;
+    }
+
+    public int day05PartTwo() {
+        int sum = 0;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Day_05 day_05 = new Day_05();
+        int answer1, answer2;
+        day_05.getInputData();
+        answer1 = day_05.day05PartOne();
+        //System.out.println(day_05.seatsBinary);
+        answer2 = day_05.day05PartTwo();
+        System.out.println("\n\nAdvent of code 2020, Day 05\n");
+        System.out.println("Solution Part one: " + answer1);
+        System.out.println("Solution Part two: " + answer2 + "\n\n");
+    }
+}
---
+++ src/test/Day_05_test.java
@@ -0,0 +1,27 @@
+package test;
+
+import junit.framework.TestCase;
+import main.Day_05;
+import org.junit.Test;
+
+public class Day_05_test extends TestCase {
+
+    Day_05 day_05 = new Day_05();
+
+    @Test
+    public void testPartOne_1() {
+        assertEquals(357, day_05.seatId("FBFBBFFRLR"));
+        assertEquals(567, day_05.seatId("BFFFBBFRRR"));
+        assertEquals(119, day_05.seatId("FFFBBBFRRR"));
+        assertEquals(820, day_05.seatId("BBFFBBFRLL"));
+    }
+
+    @Test
+    public void testSolutionPartOne() {
+
+        day_05.getInputData();
+        int expected = 801;
+        int actual = day_05.day05PartOne();
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+}
