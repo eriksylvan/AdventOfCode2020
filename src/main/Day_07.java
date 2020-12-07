f8140a07fe6225b742ad1a34199c5ccbb003ea7b
Sylvan <sylver@corp.delaval.com>
2020-12-07 23:06:09 +0100

    Solution Day 07 Part One

--- src/main/Day_07.java
+++ src/main/Day_07.java
@@ -1,6 +1,9 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -10,6 +13,17 @@ public class Day_07 {
 
     public String inputFile = "input\\input_07.txt";
     ArrayList<String> bagRules = new ArrayList<String>();
+    Map<String, List<BagContent<String, Integer>>> colorMap = new HashMap<String, List<BagContent<String, Integer>>>();
+
+    public class BagContent<T1, T2> {
+        public final T1 color;
+        public final T2 number;
+
+        private BagContent(T1 t1, T2 t2) {
+            this.color = t1;
+            this.number = t2;
+        }
+    }
 
     public void getInputData() {
         Scanner inputScanner;
@@ -19,16 +33,67 @@ public class Day_07 {
             bagRules.add(inputScanner.nextLine());
         }
         inputScanner.close();
-        System.out.println(bagRules);
+        // System.out.println(bagRules);
     }
 
     public int day07PartOne() {
-        
-        // Split on " bags contain " 
-        // regex:
-        // (.*) bags contain (.*).
-        int sum = 0;
-        return sum;
+        int count = 0;
+        int found = 0;
+        for (String str : this.bagRules)
+        {
+            
+            String[] cr = str.split(" bags contain ");
+
+            String color = cr[0];
+            List<BagContent<String, Integer>> bagContentList = new ArrayList<BagContent<String, Integer>>();
+
+            if (cr[1].startsWith("no", 0)) {
+                System.out.println(color + ": No other bags inside");
+
+            } else {
+                String[] contains = cr[1].split(", ");
+                //System.out.println(color + ": " + contains.length);
+
+                for (String s : contains) {
+                    String[] containSplit = s.split(" ");
+                    Integer n = Integer.parseInt(containSplit[0]);
+                    String col = containSplit[1] + " " + containSplit[2];
+                    //System.out.println(n + " : " + col);
+
+                    bagContentList.add(new BagContent<String, Integer>(col, n));
+                }
+               
+            }
+             this.colorMap.put(color, bagContentList);
+            //System.out.println("------");
+        }
+
+        for (String c : colorMap.keySet()) {
+            count++;
+            //System.out.println(c);
+            if (containsShinyGold(c)) {
+                System.out.println(c +  " kan innehålla ShinyGold. ");
+                found++;
+            }
+        }
+
+        System.out.print("Count: "+ count);
+        return found;
+    }
+
+    private boolean containsShinyGold(String c) {
+        boolean contains = false;
+        for (BagContent<String, Integer> child : colorMap.get(c)) {
+            if (child.color.equals("shiny gold")) {
+                contains = true;
+                break;
+            } else {
+                contains = containsShinyGold(child.color);
+                if (contains)
+                    break;
+            }
+        }
+        return contains;
     }
 
     public int day07PartTwo() {
@@ -41,9 +106,9 @@ public class Day_07 {
         int answer1, answer2;
         day_07.getInputData();
         answer1 = day_07.day07PartOne();
-        
+
         answer2 = day_07.day07PartTwo();
-        System.out.println("\n\nAdvent of code 2020, Day 07\n");    
+        System.out.println("\n\nAdvent of code 2020, Day 07\n");
         System.out.println("Solution Part one: " + answer1);
         System.out.println("Solution Part two: " + answer2 + "\n\n");
     }
--- src/test/Day_07_test.java
+++ src/test/Day_07_test.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import main.Day_07;
 import org.junit.Test;
 
+
 public class Day_07_test extends TestCase {
 
     Day_07 day_07 = new Day_07();
@@ -13,9 +14,21 @@ public class Day_07_test extends TestCase {
         day_07.inputFile = "input\\input_07_test.txt";
         day_07.getInputData();
 
-        int expected = 9999;
+        int expected = 4;
         int actual = day_07.day07PartOne();
         assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
     }
+
+    @Test
+    public void testSolutionPartOne() {
+        day_07.getInputData();
+
+        int expected = 233;
+        int actual = day_07.day07PartOne();
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+ 
+    }
+
+
 }
 
