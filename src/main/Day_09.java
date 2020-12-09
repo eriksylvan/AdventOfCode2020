51ae838342df39b8d28ca565aed2cc8c9b6b9402
Sylvan <sylver@corp.delaval.com>
2020-12-09 23:35:08 +0100

    Solution Day 09

--- src/main/Day_09.java
+++ src/main/Day_09.java
@@ -1,49 +1,118 @@
 package main;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 /**
  * Day_09
+ * https://adventofcode.com/2020/day/9
  */
 public class Day_09 {
 
     public String inputFile = "input\\input_09.txt";
-    ArrayList<String> bagRules = new ArrayList<String>();
-   
+    ArrayList<Long> numbers = new ArrayList<Long>();
+
     public void getInputData() {
         Scanner inputScanner;
         inputScanner = new Scanner(Day_09.class.getResourceAsStream(inputFile));
-        this.bagRules = new ArrayList<>();
-        while (inputScanner.hasNextLine()) {
-            bagRules.add(inputScanner.nextLine());
+        this.numbers = new ArrayList<>();
+        while (inputScanner.hasNextLong()) {
+            numbers.add(inputScanner.nextLong());
         }
         inputScanner.close();
     }
 
+    private boolean findSum(long sum, List<Long> preamble) {
+        Long s = 0l;
+        boolean found = false;
+        int l = preamble.size();
+        for (int i = 0; i < l; i++) {
+            for (int j = i + 1; j < l; j++) {
+                Long a = preamble.get(i);
+                Long b = preamble.get(j);
+                s = a + b;
+                if (s == sum) {
+                    found = true;
+                    break;
+                }
 
-    public int day09PartOne() {
-        int sum = 0;
-        return sum;
+                if (found == true)
+                    break;
+            }
+            if (found == true)
+                break;
+        }
+        return found;
     }
 
-    public int day09PartTwo() {
-        int sum = 0;
-        return sum;
+    public long day09PartOne(int n) {
+        boolean f = true;
+        long s = 0;
+
+        for (int i = n; ((i < this.numbers.size()) && f); i++) {
+            s = numbers.get(n + i);
+            f = findSum(s, numbers.subList(i, n + i));
+        }
+
+        return s;
+    }
+
+    public long day09PartTwo(long inv) {
+        ArrayList<Long> accNumbers = new ArrayList<Long>();
+        long acc = 0;
+        boolean found = false;
+        for (long n : numbers) {
+            acc += n;
+            accNumbers.add(acc);
+        }
+        int l = 0;
+        int h = this.numbers.indexOf(inv);
+        long sum = 0;
+
+        for (int a = 0; a < h - 1; a++) {
+            for (int b = a + 1; b < h; b++) {
+                sum = accNumbers.get(b) - (a <= 0 ? 0 : accNumbers.get(a - 1));
+                if (sum > inv)
+                    break;
+                if (sum == inv) {
+                    found = true;
+                    l = a;
+                    h = b;
+                    break;
+                }
+            }
+
+            if (found)
+                break;
+        }
+        long mx = Long.MIN_VALUE;
+        long mn = Long.MAX_VALUE;
+        for (long v : numbers.subList(l, h + 1)) {
+            mx = v > mx ? v : mx;
+            mn = v < mn ? v : mn;
+        }
+
+        return mx + mn;
     }
 
     public static void main(String[] args) {
+
         Day_09 day_09 = new Day_09();
-        int answer1, answer2;
-        //ArrayList<Integer> inp = day_xx.getInputData();
-        answer1 = day_09.day09PartOne();
-        
-        answer2 = day_09.day09PartTwo();
-        System.out.println("\n\nAdvent of code 2020, Day 09\n");    
+        day_09.getInputData();
+        long answer1, answer2;
+        answer1 = day_09.day09PartOne(25);
+        answer2 = day_09.day09PartTwo(answer1);
+
+        System.out.println("\n\nAdvent of code 2020, Day 09\n");
         System.out.println("Solution Part one: " + answer1);
         System.out.println("Solution Part two: " + answer2 + "\n\n");
     }
 }
+
+/*
+Advent of code 2020, Day 09
+
+Solution Part one: 21806024
+Solution Part two: 2986195
+*/
--- src/test/Day_09_test.java
+++ src/test/Day_09_test.java
@@ -10,11 +10,36 @@ public class Day_09_test extends TestCase {
 
     @Test
     public void testPartOne_1() {
-        //Integer[] input = { +1, -2, +3, +1 };
-        //ArrayList<Integer> indata = new ArrayList<Integer>(Arrays.asList(input));
+        day_09.inputFile = "input\\input_09_test.txt";
+        day_09.getInputData();
+        long expected = 127l;
+        long actual = day_09.day09PartOne(5);
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
 
-        int expected = 9999;
-        int actual = day_09.day09PartOne();
+    @Test
+    public void testPartTwo_1() {
+        day_09.inputFile = "input\\input_09_test.txt";
+        day_09.getInputData();
+        long expected = 62l;
+        long actual = day_09.day09PartTwo(127);
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+
+
+    @Test
+    public void testSolutionOne_1() {
+        day_09.getInputData();
+        long expected = 21806024L;
+        long actual = day_09.day09PartOne(25);
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+
+    @Test
+    public void testSolutionTwo_1() {
+        day_09.getInputData();
+        long expected = 2986195L;
+        long actual = day_09.day09PartTwo(21806024L);
         assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
     }
 }
